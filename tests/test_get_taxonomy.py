b157a8a506a7e2725a859d741ab9dcfacf0a0747
gbabineau <guy.babineau@gmail.com>
2023-09-09 12:51:59 -0400

    Add some coverage

--- tests/test_get_taxonomy.py
+++ tests/test_get_taxonomy.py
@@ -1,12 +1,30 @@
 """
 Tests  photo_id/get_taxonomy.py
 """
+import json
 import unittest
+from unittest import mock
 import photo_id.get_taxonomy
-
 class TestGetTaxonomy(unittest.TestCase):
 
     def test_ebird_taxonomy(self):
-        taxonomy = photo_id.get_taxonomy.ebird_taxonomy()
-        self.assertTrue(len(taxonomy)>0)
-
+        """ tests the function with that name """
+        test_json = { "name" : "value"}
+        # Test case for when does not exist
+        with mock.patch('photo_id.get_taxonomy.os.path.isfile') as mock_isfile, \
+            mock.patch("builtins.open", mock.mock_open(read_data=json.dumps(test_json))) as mock_file, \
+            mock.patch('photo_id.get_taxonomy.get_taxonomy') as mock_get_taxonomy:
+            mock_isfile.return_value = True
+            mock_get_taxonomy.return_value=test_json
+            taxonomy = photo_id.get_taxonomy.ebird_taxonomy()
+            mock_file.assert_called_with(".cache/taxonomy", encoding='utf-8', mode='rt')
+            self.assertEqual(taxonomy, test_json)
+        # Cache does not exist
+        with mock.patch('photo_id.get_taxonomy.os.path.isfile') as mock_isfile, \
+            mock.patch("builtins.open", mock.mock_open(read_data=json.dumps(test_json))) as mock_file, \
+            mock.patch('photo_id.get_taxonomy.get_taxonomy') as mock_get_taxonomy:
+            mock_isfile.return_value = False
+            mock_get_taxonomy.return_value=test_json
+            taxonomy = photo_id.get_taxonomy.ebird_taxonomy()
+            mock_file.assert_called_with(".cache/taxonomy", encoding='utf-8', mode='wt')
+            self.assertEqual(taxonomy, test_json)
